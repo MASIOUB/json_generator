cb0166e04d0ae8a86db1c57b1690b8459695dd2a
marouaneXan <marouanezahaoui835@gmail.com>
2022-09-29 10:09:01 +0100

    run server PORT 5000

--- Api/server.js
+++ Api/server.js
@@ -1,4 +1,5 @@
 const express=require('express')
 const dotenv=require('dotenv').config()
-const PORT= process.env.PORT
+const PORT= process.env.PORT || 5000
 const app=express()
+app.listen(PORT,()=>{console.log(`Server connecting ${PORT}`)})
